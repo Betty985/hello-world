69f89e7977b5a326d6db1b4e9b17e0714f51f686
Betty <61933658+Betty985@users.noreply.github.com>
2021-10-04 00:11:49 +0800

    Update 继承与多态.cs

--- 继承与多态.cs
+++ 继承与多态.cs
@@ -11,17 +11,26 @@ namespace Shape
     #region Point类
     public class Point : IShape
     {
-        int x;
-        int y;
-        int z;
+        public int x;
+        public int y;
         string name = "点";
+        public Point()
+        {
+
+        }
+        public Point(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+            
+        }
         //实现接口
         public int perimeter()
         {
             int c = 0;
             //$ 特殊字符将字符串文本标识为内插字符串
             //Console.WriteLine($"{ name}周长是{ c}");
-            Console.WriteLine($"{0} 周长是{1}", c, c);
+            Console.WriteLine($"{0} 周长是{1}", name, c);
             return c;
         }
         public int area()
@@ -34,9 +43,22 @@ namespace Shape
     }
     #endregion
     #region Line类
-    class Line : Point
+   public  class Line : Point
     {
+        public int xl;
+        public int yl;
+        string name = "线";
+        public Line()
+        {
 
+        }
+        public  Line(int x,int y,int xl,int yl)
+        {
+            this.x= x;
+            this.y = y;
+            this.xl = xl;
+            this.yl = yl;
+        }
     }
     #endregion
     class TestMain
